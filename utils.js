a4cd09c8fc6710545acfc61211ae84225b2a9240
AlenaYanova <leonayanov@gmail.com>
2018-07-01 14:46:12 +0300

    fixed

--- utils.js
+++ utils.js
@@ -1,24 +1,20 @@
-function largest() {
-    let max = undefined;
-    if (arguments[0])
-        max = arguments[0];
-    else return max;
-    for (let i=1; i< arguments.length; i++){
-        if (arguments[i] > max)
-            max = arguments[i]
-    }
+function largest(...arg) {
+    if (!arg[0]) return undefined;
+    let max = arg[0];
+    arg.map((num) => {
+        if (max < num)
+            max = num;
+    });
     return max;
 }
 
-function smallest() {
-    let min = undefined;
-    if (arguments[0])
-        min = arguments[0];
-    else return min;
-    for (let i=1; i< arguments.length; i++){
-        if (arguments[i] < min)
-            min = arguments[i]
-    }
+function smallest(...arg) {
+    if (!arg[0]) return undefined;
+    let min = arg[0];
+    arg.map((num) => {
+        if (min > num)
+            min = num;
+    });
     return min;
 }
 
@@ -26,13 +22,11 @@ function smallest() {
 // console.log(smallest(2, 0.1, -5, 100, 3));
 
 function transform(base_array) {
-    let transform_array = [];
-    for (let i=0; i < base_array.length; ++i){
-        transform_array[i] = function () {
-            return base_array[i];
+    return base_array.map((item) => {
+        return function () {
+            return item;
         }
-    }
-    return transform_array;
+    })
 }
 
 // const baseArray = [10, 20, 30, 40, 50];
@@ -42,8 +36,8 @@ function transform(base_array) {
 function sum(...arg) {
     if (arg.length === 0) return undefined;
     if (arg.length === 1) return arg[0];
-    arg[arg.length-2] += arg [arg.length-1];
-    delete arg [arg.length-1];
+    arg[arg.length-2] += arg[arg.length-1];
+    delete arg[arg.length-1];
     --arg.length;
     return sum(...arg);
 }
@@ -51,21 +45,21 @@ function sum(...arg) {
 // console.log((sum(1,3,5,7)));
 
 function countDown(n) {
-    for (let i=n; i>= 0; i--)
+    for (let i = n; i >= 0; i--)
         setTimeout(function () {
             console.log(i)
-        }, 1000*(n-i+1))
+        }, 1000*(n - i + 1))
 }
 
 // countDown(4);
 
-Function.prototype.myBind = function (context) {
+Function.prototype.myBind = function (context, ...arg) {
     if (typeof this !== "function")
         throw new TypeError("Function.prototype.myBing - 'this' is not a function");
-    const args = [].slice.call(arguments, 1);
+    const args = arg;
     const func = this;
-    return function() {
-        const funcArgs = [].slice.call(arguments, 0);
+    return function(...arg) {
+        const funcArgs = [].slice.call(arg, 0);
         return func.apply(context, args.concat(funcArgs));
     };
 };
